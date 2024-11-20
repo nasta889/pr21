8a2fef0a6dcffe4f45782922af899b06b1ec65c6
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 15:15:49 +0200

    Update Transaction.java

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,26 @@
 package PACKAGE_NAME;public class Transaction {
 }
+public class Transaction {
+}
+public class Transaction {
+    private String date;
+    private double amount;
+    private String description;
+
+    public Transaction(String date, double amount, String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    // Getters
+    // Setters
+    // toString метод для зручності виводу
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date='" + date + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
